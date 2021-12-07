vkochan/borsch

commit 684751265a09d1dd3b62b923a4fa9e552f525139

ui: unify windows drawing for vt and text buffers
Draw windows by using ui api instead of ncurses api,
so vt and text windows uses same drawing infra.

Absolute drawings still uses ncurses api (except layout
arragne funcs).

Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

ui/term.c
@@ -37,6 +37,7 @@ typedef struct {
 typedef struct {
 	UiWin win;
 	WINDOW *cwin;
+	int cur_x, cur_y;
 } WinTerm;
 
 static bool is_utf8, has_default_colors;
@@ -204,6 +205,51 @@ static void term_window_move(UiWin *win, int x, int y)
 	mvwin(twin->cwin, y, x);
 }
 
+void term_window_text_color_set(UiWin *win, short fg, short bg)
+{
+	WinTerm *twin = (WinTerm*)win;
+
+	wcolor_set(twin->cwin, ui_window_color_get(win, fg, bg), NULL);
+}
+
+void term_window_text_attr_set(UiWin *win, unsigned attrs)
+{
+	WinTerm *twin = (WinTerm*)win;
+
+	wattrset(twin->cwin, attrs);
+}
+
+void term_window_draw_char(UiWin *win, int x, int y, unsigned int ch, int n)
+{
+	WinTerm *twin = (WinTerm*)win;
+
+	mvwhline(twin->cwin, y, x, ch, n);
+}
+
+void term_window_draw_text(UiWin *win, int x, int y, const char *text, int n)
+{
+	WinTerm *twin = (WinTerm*)win;
+
+	mvwaddnstr(twin->cwin, y, x, text, n);
+}
+
+static void term_window_cursor_set(UiWin *win, int x, int y)
+{
+	WinTerm *twin = (WinTerm*)win;
+
+	wmove(twin->cwin, y, x);
+	twin->cur_x = x;
+	twin->cur_y = y;
+}
+
+static void term_window_cursor_get(UiWin *win, int *x, int *y)
+{
+	WinTerm *twin = (WinTerm*)win;
+
+	*x = twin->cur_x;
+	*y = twin->cur_y;
+}
+
 static void term_window_draw(UiWin *win)
 {
 	const Line *line = view_lines_first(win->view);
@@ -223,6 +269,13 @@ static void term_window_draw(UiWin *win)
 			waddnstr(twin->cwin, c->data, c->len);
 		}
 	}
+
+	wnoutrefresh(twin->cwin);
+}
+
+static void term_window_refresh(UiWin *win)
+{
+	WinTerm *twin = (WinTerm*)win;
 
 	wnoutrefresh(twin->cwin);
 }
@@ -248,10 +301,18 @@ Ui *ui_term_new(void)
 	tui->ui.draw_char_vert = term_draw_char_vert;
 	tui->ui.window_new = term_window_new;
 	tui->ui.window_free = term_window_free;
+	tui->ui.window_cursor_set = term_window_cursor_set;
+	tui->ui.window_cursor_get = term_window_cursor_get;
 	tui->ui.window_draw = term_window_draw;
 	tui->ui.window_redraw = term_window_redraw;
+	tui->ui.window_refresh = term_window_refresh;
 	tui->ui.window_resize = term_window_resize;
 	tui->ui.window_move = term_window_move;
+	tui->ui.window_text_color_set = term_window_text_color_set;
+	tui->ui.window_text_attr_set = term_window_text_attr_set;
+	tui->ui.window_draw_char = term_window_draw_char;
+	tui->ui.window_draw_text = term_window_draw_text;
+
 	tui->ui.window_color_get = term_window_color_get;
 
 	return tui;

main.c
@@ -68,7 +68,6 @@ typedef struct {
 
 typedef struct Client Client;
 struct Client {
-	WINDOW *window;
 	Buffer *buf;
 	View *view;
 	UiWin *win;
@@ -632,11 +631,10 @@ static void
 draw_border(Client *c) {
 	int attrs_title = (COLOR(MAGENTA) | A_NORMAL);
 	int x, y, maxlen, attrs = NORMAL_ATTR;
+	char title[256];
+	size_t len;
 	char t = '\0';
 
-
-	if (!c->pid)
-		return;
 	if (!show_border())
 		return;
 	if (sel != c && c->urgent)
@@ -644,14 +642,14 @@ draw_border(Client *c) {
 	if (sel == c || (pertag.runinall[pertag.curtag] && !c->minimized))
 		attrs_title = attrs = SELECTED_ATTR;
 
-	wattrset(c->window, attrs);
-	getyx(c->window, y, x);
+	ui_window_text_attr_set(c->win, attrs);
+	ui_window_cursor_get(c->win, &x, &y);
 	if (c == sel) {
-	        wattrset(c->window, COLOR(BLUE_BG));
-		mvwhline(c->window, 0, 0, ' ', c->w);
-	        wattrset(c->window, attrs);
+		ui_window_text_attr_set(c->win, COLOR(BLUE_BG));
+		ui_window_draw_char(c->win, 0, 0, ' ', c->w);
+		ui_window_text_attr_set(c->win, attrs);
 	} else {
-		mvwhline(c->window, 0, 0, ACS_HLINE, c->w); }
+		ui_window_draw_char(c->win, 0, 0, ACS_HLINE, c->w); }
 	maxlen = c->w - 10;
 	if (maxlen < 0)
 		maxlen = 0;
@@ -660,39 +658,35 @@ draw_border(Client *c) {
 		c->title[maxlen] = '\0';
 	}
 
-	wattrset(c->window, attrs_title);
-	mvwprintw(c->window, 0, 2, "[%d|%s%s]",
-		  c->order,
-		  ismastersticky(c) ? "*" : "",
-	          *c->title ? c->title : "");
-	wattrset(c->window, attrs);
+	ui_window_text_attr_set(c->win, attrs_title);
+	len = snprintf(title, sizeof(title), "[%d|%s%s]",
+			c->order,
+			ismastersticky(c) ? "*" : "",
+			*c->title ? c->title : "");
+	ui_window_draw_text(c->win, 2, 0, title, len);
+	ui_window_text_attr_set(c->win, attrs);
 	if (t)
 		c->title[maxlen] = t;
-	wmove(c->window, y, x);
+	ui_window_cursor_set(c->win, x, y);
 }
 
 static void
 draw_content(Client *c) {
 	if (c->pid)
-		vt_draw(c->term, c->window, c->has_title_line, 0);
+		vt_draw(c->term, c->win, c->has_title_line, 0);
 	else
 		ui_window_draw(c->win);
 }
 
 static void
 draw(Client *c) {
-	if (!c->pid) {
-		draw_content(c);
-		return;
-	}
-
 	if (is_content_visible(c)) {
-		redrawwin(c->window);
+		ui_window_redraw(c->win);
 		draw_content(c);
 	}
 	if (!isarrange(fullscreen) || c == sel)
 		draw_border(c);
-	wnoutrefresh(c->window);
+	ui_window_refresh(c->win);
 }
 
 static void
@@ -882,8 +876,7 @@ focus(Client *c) {
 		lastsel->urgent = false;
 		if (!isarrange(fullscreen)) {
 			draw_border(lastsel);
-			if (lastsel->pid)
-				wnoutrefresh(lastsel->window);
+			ui_window_refresh(lastsel->win);
 		}
 	}
 
@@ -896,9 +889,10 @@ focus(Client *c) {
 		c->urgent = false;
 		if (isarrange(fullscreen)) {
 			draw(c);
-		} else if (c->pid) {
+		/* } else if (c->pid) { */
+		} else {
 			draw_border(c);
-			wnoutrefresh(c->window);
+			ui_window_refresh(c->win);
 		}
 
 		if (c->pid)
@@ -965,19 +959,9 @@ static void
 move_client(Client *c, int x, int y) {
 	if (c->x == x && c->y == y)
 		return;
-	if (!c->pid) {
-		ui_window_move(c->win, x, y);
-		c->x = x;
-		c->y = y;
-		return;
-	}
-	debug("moving, x: %d y: %d\n", x, y);
-	if (mvwin(c->window, y, x) == ERR) {
-		eprint("error moving, x: %d y: %d\n", x, y);
-	} else {
-		c->x = x;
-		c->y = y;
-	}
+	ui_window_move(c->win, x, y);
+	c->x = x;
+	c->y = y;
 }
 
 static void
@@ -985,22 +969,9 @@ resize_client(Client *c, int w, int h) {
 	bool has_title_line = show_border();
 	bool resize_window = c->w != w || c->h != h;
 	if (resize_window) {
-		if (!c->pid) {
-			ui_window_resize(c->win, w, h - has_title_line);
+			ui_window_resize(c->win, w, h);
 			c->w = w;
 			c->h = h;
-			return;
-		}
-	}
-
-	if (resize_window) {
-		debug("resizing, w: %d h: %d\n", w, h);
-		if (wresize(c->window, h, w) == ERR) {
-			eprint("error resizing, w: %d h: %d\n", w, h);
-		} else {
-			c->w = w;
-			c->h = h;
-		}
 	}
 	if (resize_window || c->has_title_line != has_title_line) {
 		c->has_title_line = has_title_line;
@@ -1351,14 +1322,9 @@ destroy(Client *c) {
 	}
 	if (lastsel == c)
 		lastsel = NULL;
-	if (c->pid) {
-		werase(c->window);
-		wnoutrefresh(c->window);
+	if (c->pid)
 		vt_destroy(c->term);
-		delwin(c->window);
-	} else {
-		ui_window_free(c->win);
-	}
+	ui_window_free(c->win);
 	view_free(c->view);
 	buffer_del(c->buf);
 
@@ -1469,14 +1435,8 @@ int create(const char *prog, const char *title, const char *cwd) {
 	c->id = ++cmdfifo.id;
 	snprintf(buf, sizeof buf, "%d", c->id);
 
-	if (!(c->window = newwin(wah, waw, way, wax))) {
-		free(c);
-		return -1;
-	}
-
 	c->buf = buffer_new();
 	if (!c->buf) {
-		delwin(c->window);
 		free(c);
 		return -1;
 	}
@@ -1484,7 +1444,6 @@ int create(const char *prog, const char *title, const char *cwd) {
 	c->view = view_new(buffer_text_get(c->buf));
 	if (!c->view) {
 		buffer_del(c->buf);
-		delwin(c->window);
 		free(c);
 	}
 
@@ -1495,12 +1454,13 @@ int create(const char *prog, const char *title, const char *cwd) {
 		free(c);
 		return -1;
 	}
+	ui_window_resize(c->win, waw, wah);
+	ui_window_move(c->win, wax, way);
 
 	c->term = c->app = vt_create(c->win, screen.h, screen.w, screen.history);
 	if (!c->term) {
 		view_free(c->view);
 		buffer_del(c->buf);
-		delwin(c->window);
 		free(c);
 		return -1;
 	}
@@ -1703,13 +1663,9 @@ static void
 redraw(const char *args[]) {
 	for (Client *c = clients; c; c = c->next) {
 		if (!c->minimized) {
-			if (c->pid) {
+			if (c->pid)
 				vt_dirty(c->term);
-				wclear(c->window);
-				wnoutrefresh(c->window);
-			} else {
-				ui_window_redraw(c->win);
-			}
+			ui_window_redraw(c->win);
 		}
 	}
 	resize_screen();
@@ -2132,7 +2088,7 @@ handle_overlay(Client *c) {
 	c->term = c->app;
 	vt_dirty(c->term);
 	draw_content(c);
-	wnoutrefresh(c->window);
+	ui_window_refresh(c->win);
 }
 
 static int
@@ -2312,14 +2268,12 @@ rescan:
 					synctitle(c);
 				if (c != sel) {
 					draw_content(c);
-					if (c->pid)
-						wnoutrefresh(c->window);
+					ui_window_refresh(c->win);
 				}
 			} else if (!isarrange(fullscreen) && isvisible(c)
 					&& c->minimized) {
 				draw_border(c);
-				if (c->pid)
-					wnoutrefresh(c->window);
+				ui_window_refresh(c->win);
 			}
 		}
 
@@ -2327,7 +2281,7 @@ rescan:
 			draw_content(sel);
 			if (sel->pid) {
 				curs_set(vt_cursor_visible(sel->term));
-				wnoutrefresh(sel->window);
+				ui_window_refresh(sel->win);
 			}
 		}
 	}

ui/core.c
@@ -63,6 +63,18 @@ void ui_window_free(UiWin *win)
 		free(win);
 }
 
+void ui_window_cursor_set(UiWin *win, int x, int y)
+{
+	if (win->ui->window_cursor_set)
+		win->ui->window_cursor_set(win, x, y);
+}
+
+void ui_window_cursor_get(UiWin *win, int *x, int *y)
+{
+	if (win->ui->window_cursor_get)
+		win->ui->window_cursor_get(win, x, y);
+}
+
 void ui_window_draw(UiWin *win)
 {
 	view_draw(win->view);
@@ -75,6 +87,12 @@ void ui_window_redraw(UiWin *win)
 {
 	if (win->ui->window_redraw)
 		win->ui->window_redraw(win);
+}
+
+void ui_window_refresh(UiWin *win)
+{
+	if (win->ui->window_refresh)
+		win->ui->window_refresh(win);
 }
 
 void ui_window_resize(UiWin *win, int width, int height)
@@ -92,6 +110,30 @@ void ui_window_move(UiWin *win, int x, int y)
 
 	if (win->ui->window_move)
 		win->ui->window_move(win, x, y);
+}
+
+void ui_window_text_color_set(UiWin *win, short fg, short bg)
+{
+	if (win->ui->window_text_color_set)
+		win->ui->window_text_color_set(win, fg, bg);
+}
+
+void ui_window_text_attr_set(UiWin *win, unsigned attrs)
+{
+	if (win->ui->window_text_attr_set)
+		win->ui->window_text_attr_set(win, attrs);
+}
+
+void ui_window_draw_char(UiWin *win, int x, int y, unsigned int ch, int n)
+{
+	if (win->ui->window_draw_char)
+		win->ui->window_draw_char(win, x, y, ch, n);
+}
+
+void ui_window_draw_text(UiWin *win, int x, int y, const char *text, int n)
+{
+	if (win->ui->window_draw_text)
+		win->ui->window_draw_text(win, x, y, text, n);
 }
 
 short ui_window_color_get(UiWin *win, short fg, short bg)

ui/ui.h
@@ -87,10 +87,17 @@ struct Ui {
 	void (*resize)(Ui*);
 	UiWin* (*window_new)(Ui*, View *view);
 	void (*window_free)(UiWin*);
+	void (*window_cursor_set)(UiWin*, int x, int y);
+	void (*window_cursor_get)(UiWin*, int *x, int *y);
 	void (*window_draw)(UiWin*);
 	void (*window_redraw)(UiWin*);
+	void (*window_refresh)(UiWin*);
 	void (*window_resize)(UiWin*, int width, int height);
 	void (*window_move)(UiWin*, int x, int y);
+	void (*window_text_color_set)(UiWin*, short fg, short bg);
+	void (*window_text_attr_set)(UiWin*, unsigned attrs);
+	void (*window_draw_char)(UiWin*, int x, int y, unsigned int ch, int n);
+	void (*window_draw_text)(UiWin *win, int x, int y, const char *text, int n);
 	short (*window_color_get)(UiWin *win, short fg, short bg);
 	void (*arrange)(Ui*, enum UiLayout);
 	void (*draw)(Ui*);
@@ -128,8 +135,15 @@ void ui_draw_char(Ui *ui, int x, int y, unsigned int ch, int n);
 void ui_draw_char_vert(Ui *ui, int x, int y, unsigned int ch, int n);
 UiWin *ui_window_new(Ui *ui, View *view);
 void ui_window_free(UiWin *win);
+void ui_window_cursor_set(UiWin *win, int x, int y);
+void ui_window_cursor_get(UiWin *win, int *x, int *y);
+void ui_window_text_color_set(UiWin *win, short fg, short bg);
+void ui_window_text_attr_set(UiWin *win, unsigned attrs);
 void ui_window_draw(UiWin *win);
+void ui_window_draw_char(UiWin *win, int x, int y, unsigned int ch, int n);
+void ui_window_draw_text(UiWin *win, int x, int y, const char *text, int n);
 void ui_window_redraw(UiWin *win);
+void ui_window_refresh(UiWin *win);
 void ui_window_resize(UiWin *win, int width, int height);
 void ui_window_move(UiWin *win, int x, int y);
 short ui_window_color_get(UiWin *win, short fg, short bg);

vt.c
@@ -1492,9 +1492,10 @@ void vt_dirty(Vt *t)
 		row->dirty = true;
 }
 
-void vt_draw(Vt *t, WINDOW *win, int srow, int scol)
+void vt_draw(Vt *t, UiWin *win, int srow, int scol)
 {
 	VtBuffer *b = t->buffer;
+	int i, j;
 
 	if (srow != t->srow || scol != t->scol) {
 		vt_dirty(t);
@@ -1502,15 +1503,15 @@ void vt_draw(Vt *t, WINDOW *win, int srow, int scol)
 		t->scol = scol;
 	}
 
-	for (int i = 0; i < b->rows; i++) {
+	for (i = 0; i < b->rows; i++) {
 		VtRow *row = b->lines + i;
 
 		if (!row->dirty)
 			continue;
 
-		wmove(win, srow + i, scol);
+		ui_window_cursor_set(win, scol, srow + i);
 		VtCell *cell = NULL;
-		for (int j = 0; j < b->cols; j++) {
+		for (j = 0; j < b->cols; j++) {
 			VtCell *prev_cell = cell;
 			cell = row->cells + j;
 			if (!prev_cell || cell->attr != prev_cell->attr
@@ -1522,33 +1523,30 @@ void vt_draw(Vt *t, WINDOW *win, int srow, int scol)
 					cell->fg = ui_window_default_fg_get(t->win);
 				if (cell->bg == -1)
 					cell->bg = ui_window_default_bg_get(t->win);
-				wattrset(win, cell->attr << NCURSES_ATTR_SHIFT);
-				wcolor_set(win, ui_window_color_get(t->win, cell->fg, cell->bg), NULL);
+				ui_window_text_attr_set(win, cell->attr << NCURSES_ATTR_SHIFT);
+				ui_window_text_color_set(win, cell->fg, cell->bg);
 			}
 
 			if (is_utf8 && cell->text >= 128) {
 				char buf[MB_CUR_MAX + 1];
 				size_t len = wcrtomb(buf, cell->text, NULL);
 				if (len > 0) {
-					mvwaddnstr(win, srow + i, j, buf, len);
+					ui_window_draw_text(win, j, srow + i, buf, len);
 					if (wcwidth(cell->text) > 1)
 						j++;
 				}
 			} else {
-				mvwaddch(win, srow + i, j, cell->text > ' ' ? cell->text : ' ');
+				ui_window_draw_char(win, j, srow + i, cell->text > ' ' ? cell->text : ' ', 1);
 			}
 		}
 
-		int x, y;
-		getyx(win, y, x);
-		(void)y;
-		if (x && x < b->cols - 1)
-			mvwhline(win, x, y, ' ', b->cols - x);
+		if (j && j < b->cols - 1)
+			ui_window_draw_char(win, j, srow + i, ' ', b->cols - j);
 
 		row->dirty = false;
 	}
 
-	wmove(win, srow + b->curs_row - b->lines, scol + b->curs_col);
+	ui_window_cursor_set(win, scol + b->curs_col, srow + b->curs_row - b->lines);
 }
 
 void vt_scroll(Vt *t, int rows)

vt.h
@@ -54,7 +54,7 @@ void vt_keypress(Vt *, int keycode);
 ssize_t vt_write(Vt*, const char *buf, size_t len);
 void vt_mouse(Vt*, int x, int y, mmask_t mask);
 void vt_dirty(Vt*);
-void vt_draw(Vt*, WINDOW *win, int startrow, int startcol);
+void vt_draw(Vt*, UiWin *win, int startrow, int startcol);
 
 void vt_scroll(Vt*, int rows);
 void vt_noscroll(Vt*);

commit f131194ae5cea4ec698f4e52e447b97adce0f12b

ui: term: fix vt is cleared if tag or layout changed
Fixes: 684751265a09 ("ui: unify windows drawing for vt and text buffers")
Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

ui/term.c
@@ -345,8 +345,7 @@ static void term_window_redraw(UiWin *win)
 {
 	WinTerm *twin = (WinTerm*)win;
 
-	wclear(twin->cwin);
-	wnoutrefresh(twin->cwin);
+	redrawwin(twin->cwin);
 }
 
 Ui *ui_term_new(void)