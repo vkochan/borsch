vkochan/borsch

commit 65ff13a152974d0169df1b227a754b697b381375

ui: core: add getter for custom window draw callback
Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

ui/core.c
@@ -46,6 +46,7 @@ UiWin *ui_window_new(Ui *ui, View *view)
 	if (ui->window_new) {
 		win = ui->window_new(ui, view);
 		if (win) {
+			win->draw = ui_window_draw;
 			win->deffg = -1;
 			win->defbg = -1;
 			win->view = view;
@@ -210,7 +211,12 @@ void *ui_window_priv_get(UiWin *win)
 	return win->priv;
 }
 
-void ui_window_draw_set(UiWin *win, void (*fn)(UiWin *))
+void ui_window_ops_draw_set(UiWin *win, void (*fn)(UiWin *))
 {
 	win->draw = fn;
+}
+
+void *ui_window_ops_draw_get(UiWin *win)
+{
+	return win->draw;
 }

ui/ui.h
@@ -89,7 +89,7 @@ struct Ui {
 	void (*window_free)(UiWin*);
 	void (*window_cursor_set)(UiWin*, int x, int y);
 	void (*window_cursor_get)(UiWin*, int *x, int *y);
-	void (*window_draw)(UiWin*);
+	void (*window_draw)(UiWin *);
 	void (*window_redraw)(UiWin*);
 	void (*window_refresh)(UiWin*);
 	void (*window_resize)(UiWin*, int width, int height);
@@ -159,6 +159,9 @@ int ui_window_x_get(UiWin *win);
 int ui_window_y_get(UiWin *win);
 void ui_window_priv_set(UiWin *win, void *priv);
 void *ui_window_priv_get(UiWin *win);
-void ui_window_draw_set(UiWin *win, void (*fn)(UiWin *));
+void ui_window_ops_draw_set(UiWin *win, void (*fn)(UiWin *));
+void *ui_window_ops_draw_get(UiWin *win);
+
+
 
 #endif

vt.c
@@ -1458,7 +1458,7 @@ Vt *vt_create(UiWin *win, int rows, int cols, int scroll_size)
 		return NULL;
 	}
 
-	ui_window_draw_set(win, vt_draw);
+	ui_window_ops_draw_set(win, vt_draw);
 	ui_window_priv_set(win, t);
 
 	return t;

commit 4a4d46f6e4ec267d5fc36ac2fe9ebb6ed147ff81

ui: core: fix infinite winow_draw call for text buffer
Remove setting of ui_window_draw as custom cb to win->draw.

Fixes: 65ff13a15297 ("ui: core: add getter for custom window draw callback")
Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

ui/core.c
@@ -72,7 +72,6 @@ UiWin *ui_window_new(Ui *ui, View *view)
 	if (ui->window_new) {
 		win = ui->window_new(ui, view);
 		if (win) {
-			win->draw = ui_window_draw;
 			win->deffg = -1;
 			win->defbg = -1;
 			win->view = view;