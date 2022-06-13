vkochan/borsch

commit 506bf32bd1c367afd349e7562f8a9ce78d373eab

keymap: convert ALT and CTRL modifiers into a flags

main.c
@@ -1965,12 +1965,17 @@ static KeyMap *buf_keymap_get(Buffer *buf)
 	return buffer_keymap_get(buf);
 }
 
-static bool keybuf_enqueue(KeyBuf *kbuf, int key)
+static bool keybuf_enqueue(KeyBuf *kbuf, int code, int flags)
 {
+	KeyCode *key;
+
 	if (kbuf->key_index >= MAX_KEYS)
 		return false;
 
-	kbuf->keys[kbuf->key_index++].code = key;
+	key = &kbuf->keys[kbuf->key_index++];
+
+	key->flags = flags;
+	key->code = code;
 	return true;
 }
 
@@ -1983,7 +1988,15 @@ static void keybuf_clear(KeyBuf *kbuf)
 static void keybuf_flush(KeyBuf *kbuf)
 {
 	for (int i = 0; i < kbuf->key_index; i++) {
-		keypress(kbuf->keys[i].code);
+		KeyCode *key = &kbuf->keys[i];
+		int code = key->code;
+
+		if (key->flags & KEY_MOD_F_CTL)
+			code = CTRL(code);
+		else if (key->flags & KEY_MOD_F_ALT)
+			keypress(ALT);
+
+		keypress(code);
 	}
 
 	keybuf_clear(kbuf);
@@ -1995,6 +2008,7 @@ static void handle_keypress(int fd, void *arg)
 	KeyBuf *kbuf = arg;
 	int alt_code;
 	event_t evt;
+	int flags;
 	int code;
 
 	if (!current_window()) {
@@ -2007,6 +2021,7 @@ static void handle_keypress(int fd, void *arg)
 
 reenter:
 	code = getch();
+	flags = 0;
 
 	evt.eid = EVT_KEY_PRESS;
 	evt.oid = code;
@@ -2017,12 +2032,12 @@ reenter:
 		alt_code = getch();
 		nodelay(stdscr, FALSE);
 		if (alt_code > 0) {
-			if (!keybuf_enqueue(kbuf, code)) {
-				keybuf_flush(kbuf);
-				return;	
-			}
+			flags |= KEY_MOD_F_ALT;
 			code = alt_code;
 		}
+	} else if (code < 0x1f) {
+		flags |= KEY_MOD_F_CTL;
+		code = code + 0x60;
 	}
 
 	if (code < 0)
@@ -2034,7 +2049,7 @@ reenter:
 	}
 
 
-	if (!keybuf_enqueue(kbuf, code)) {
+	if (!keybuf_enqueue(kbuf, code, flags)) {
 		keybuf_flush(kbuf);
 		return;	
 	}

keymap.c
@@ -9,15 +9,6 @@
 
 #define MAX_KEYS 3
 
-#define ALT	27
-#if defined CTRL && defined _AIX
-  #undef CTRL
-#endif
-#ifndef CTRL
-  #define CTRL(k)   ((k) & 0x1F)
-#endif
-#define CTRL_ALT(k) ((k) + (129 - 'a'))
-
 static void* (*symb_resolver)(keymap_symb_t type, char *name);
 
 typedef struct KeyMap KeyMap;
@@ -99,9 +90,10 @@ static int keymap_parse(KeyBinding *kbd, char *key)
 
 	for (i = 0; i < MAX_KEYS && tok; i++) {
 		if (strlen(tok) >= 3 && tok[0] == 'C' && tok[1] == '-') {
-			kbd->keys[i].code = CTRL(__keymap_code(&tok[2]));
+			kbd->keys[i].flags = KEY_MOD_F_CTL;
+			kbd->keys[i].code = __keymap_code(&tok[2]);
 		} else if (strlen(tok) >= 3 && tok[0] == 'M' && tok[1] == '-') {
-			kbd->keys[i++].code = ALT;
+			kbd->keys[i].flags = KEY_MOD_F_ALT;
 			kbd->keys[i].code = __keymap_code(&tok[2]);
 		} else {
 			kbd->keys[i].code = __keymap_code(tok);;
@@ -112,6 +104,11 @@ static int keymap_parse(KeyBinding *kbd, char *key)
 
 	kbd->len = i;
 	return 0;
+}
+
+static bool keycode_equal(KeyCode *k1, KeyCode *k2)
+{
+	return k1->code == k2->code && k1->flags == k2->flags;
 }
 
 KeyBinding *keymap_find(KeyMap *map, char *key)
@@ -129,7 +126,7 @@ KeyBinding *keymap_find(KeyMap *map, char *key)
 			continue;
 
 		for (i = 0; i < len; i++)
-			if (it->keys[i].code == kbd.keys[i].code)
+			if (keycode_equal(&it->keys[i], &kbd.keys[i]))
 				match++;
 
 		if (match == kbd.len)
@@ -151,7 +148,7 @@ KeyBinding *keymap_match(KeyMap *map, KeyCode *keys, int len)
 
 	for (it = map->kbd_list; it; it = it->next) {
 		for (m = i = 0; i < len; i++) {
-			if (it->keys[i].code == keys[i].code)
+			if (keycode_equal(&it->keys[i], &keys[i]))
 				m++;
 		}
 

keymap.h
@@ -11,7 +11,11 @@ typedef enum {
 typedef struct KeyBinding KeyBinding;
 typedef struct KeyMap KeyMap;
 
+#define KEY_MOD_F_ALT		1
+#define KEY_MOD_F_CTL		2
+
 typedef struct {
+	int flags;
 	int code;
 } KeyCode;
 

commit ff26bc1d2ecd993bd8ad2fb1b3cce3d08f8b0af0

main: fix Enter key handled with CTRL modifier
Fixes: 506bf32bd1c3 ("keymap: convert ALT and CTRL modifiers into a flags")

main.c
@@ -2035,7 +2035,7 @@ reenter:
 			flags |= KEY_MOD_F_ALT;
 			code = alt_code;
 		}
-	} else if (code < 0x1f) {
+	} else if (code < 0x1f && code != 0xd) {
 		flags |= KEY_MOD_F_CTL;
 		code = code + 0x60;
 	}