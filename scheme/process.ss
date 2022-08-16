vkochan/borsch

commit 0ac687fce00176a2f65b15b819ca4464fa6a6d11

scheme: process: kill process if any of the buffers does not exist

scheme/process.ss
@@ -9,6 +9,8 @@
 
 (define __cs_process_del (foreign-procedure "cs_process_del" (int) void))
 
+(define __cs_process_kill (foreign-procedure "cs_process_kill" (int) void))
+
 (define __cs_evt_fd_handler_add (foreign-procedure __collect_safe "cs_evt_fd_handler_add" (int void*) int))
 
 (define __cs_evt_fd_handler_del (foreign-procedure __collect_safe "cs_evt_fd_handler_del" (int) void))
@@ -27,6 +29,18 @@
 (define %process-pid-ht (make-eq-hashtable))
 (define %process-fd-ht (make-eq-hashtable))
 
+(define process-is-alive?
+   (lambda (pid)
+      (call-foreign (__cs_process_is_alive pid))
+   )
+)
+
+(define process-kill
+   (lambda (pid)
+      (call-foreign (__cs_process_kill pid))
+   )
+)
+
 (define __process-on-read-async
    (lambda (proc)
       (let (
@@ -35,10 +49,16 @@
            )
          (let ([s (get-string-some proc-out)])
             (when (not (eof-object? s))
-               (with-current-buffer buf
-                  (save-cursor
-                     (move-buffer-end)
-                     (insert s)
+               (if (buffer-is-valid? buf)
+                  (with-current-buffer buf
+                     (save-cursor
+                        (move-buffer-end)
+                        (insert s)
+                     )
+                  )
+                  ;; else
+                  (begin
+                     (process-kill (process-pid proc))
                   )
                )
             )
@@ -269,10 +289,15 @@
 
 (define on-process-exit
    (lambda (pid)
-      (let ([proc (hashtable-ref %process-pid-ht pid #f)])
+      (let (
+            [proc (hashtable-ref %process-pid-ht pid #f)]
+            [alive? (process-is-alive? pid)]
+           )
          (when proc
             (when (process-buffer-out proc)
-               (__process-on-read-sync (process-port-out proc) (process-buffer-out proc))
+               (when alive?
+                  (__process-on-read-sync (process-port-out proc) (process-buffer-out proc))
+               )
                (let ([fd (port-file-descriptor (process-port-out proc))])
                   (hashtable-delete! %process-fd-ht fd)
                   (call-foreign (__cs_evt_fd_handler_del fd))
@@ -280,7 +305,9 @@
                (close-port (process-port-out proc))
             )
             (when (process-buffer-err proc)
-               (__process-on-read-sync (process-port-err proc) (process-buffer-err proc))
+               (when alive?
+                  (__process-on-read-sync (process-port-err proc) (process-buffer-err proc))
+               )
                (let ([fd (port-file-descriptor (process-port-err proc))])
                   (hashtable-delete! %process-fd-ht fd)
                   (call-foreign (__cs_evt_fd_handler_del fd))
@@ -289,10 +316,12 @@
             )
             (close-port (process-port-in proc))
             (when (process-on-exit proc)
-               ((process-on-exit proc)
-                   (process-status pid)
-                   (process-buffer-out proc)
-                   (process-buffer-err proc)
+               (when alive?
+                  ((process-on-exit proc)
+                      (process-status pid)
+                      (process-buffer-out proc)
+                      (process-buffer-err proc)
+                  )
                )
                (unlock-object (process-cb proc))
             )

api.h
@@ -207,6 +207,7 @@ bool proc_is_alive(pid_t pid);
 int proc_status_get(pid_t pid);
 int proc_wait(pid_t pid, int *status);
 void proc_del(pid_t pid);
+void proc_kill(pid_t pid);
 
 void do_quit(void);
 

main.c
@@ -547,6 +547,11 @@ int process_kill(Process *proc)
 	return status;
 }
 
+void process_kill_async(Process *proc)
+{
+	kill(-proc->pid, SIGKILL);
+}
+
 void process_destroy(Process *proc)
 {
 	if (proc->buf)
@@ -4763,6 +4768,15 @@ void proc_del(pid_t pid)
 
 	if (proc) {
 		process_destroy(proc);
+	}
+}
+
+void proc_kill(pid_t pid)
+{
+	Process *proc = process_by_pid(pid);
+
+	if (proc) {
+		process_kill_async(proc);
 	}
 }
 

scheme.c
@@ -1061,6 +1061,11 @@ void scheme_process_delete(int pid)
 	proc_del(pid);
 }
 
+void scheme_process_kill(int pid)
+{
+	proc_kill(pid);
+}
+
 ptr scheme_process_wait(int pid)
 {
 	int status = -1;
@@ -1242,6 +1247,7 @@ static void scheme_export_symbols(void)
 	Sregister_symbol("cs_process_status_get", scheme_process_status_get);
 	Sregister_symbol("cs_process_create", scheme_process_create);
 	Sregister_symbol("cs_process_del", scheme_process_delete);
+	Sregister_symbol("cs_process_kill", scheme_process_kill);
 	Sregister_symbol("cs_process_wait", scheme_process_wait);
 
 	Sregister_symbol("cs_do_quit", scheme_do_quit);

commit 327321cb2d2cf83877cfaad4ccc4ffaf2bf5aceb

scheme: process: fix last output from the process did not read
Remove stupid alive? checking after process did exit.

Fixes: 0ac687fce001 ("scheme: process: kill process if any of the buffers does not exist")

scheme/process.ss
@@ -122,15 +122,17 @@
             [proc-out proc-out]
             [buf      buf]
            )
-         (let ([s (get-string-some proc-out)])
-            (while (not (eof-object? s))
-               (with-current-buffer buf
-                  (save-cursor
-                     (move-buffer-end)
-                     (insert s)
+         (when (buffer-is-valid? buf)
+            (let ([s (get-string-some proc-out)])
+               (while (not (eof-object? s))
+                  (with-current-buffer buf
+                     (save-cursor
+                        (move-buffer-end)
+                        (insert s)
+                     )
                   )
+                  (set! s (get-string-some proc-out))
                )
-               (set! s (get-string-some proc-out))
             )
          )
       )
@@ -336,13 +338,10 @@
    (lambda (pid)
       (let (
             [proc (hashtable-ref %process-pid-ht pid #f)]
-            [alive? (process-is-alive? pid)]
            )
          (when proc
             (when (process-buffer-out proc)
-               (when alive?
-                  (__process-on-read-sync (process-port-out proc) (process-buffer-out proc))
-               )
+               (__process-on-read-sync (process-port-out proc) (process-buffer-out proc))
                (let ([fd (port-file-descriptor (process-port-out proc))])
                   (hashtable-delete! %process-fd-ht fd)
                   (call-foreign (__cs_evt_fd_handler_del fd))
@@ -350,9 +349,7 @@
                (close-port (process-port-out proc))
             )
             (when (process-buffer-err proc)
-               (when alive?
-                  (__process-on-read-sync (process-port-err proc) (process-buffer-err proc))
-               )
+               (__process-on-read-sync (process-port-err proc) (process-buffer-err proc))
                (let ([fd (port-file-descriptor (process-port-err proc))])
                   (hashtable-delete! %process-fd-ht fd)
                   (call-foreign (__cs_evt_fd_handler_del fd))
@@ -361,12 +358,10 @@
             )
             (close-port (process-port-in proc))
             (when (process-on-exit proc)
-               (when alive?
-                  ((process-on-exit proc)
-                      (process-status pid)
-                      (process-buffer-out proc)
-                      (process-buffer-err proc)
-                  )
+               ((process-on-exit proc)
+                   (process-status pid)
+                   (process-buffer-out proc)
+                   (process-buffer-err proc)
                )
             )
             (when (process-port-reader proc)