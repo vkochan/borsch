7062f13f08b02b4d1d3beedb62efacecdeda0f05
Vadym Kochan <vadim4j@gmail.com>
2022-12-27 18:41:51 +0200

    remove layout drawing *.c modules

--- fullscreen.c
+++
@@ -1,5 +0,0 @@
-static void fullscreen(unsigned int wax, unsigned int way, unsigned int waw, unsigned int wah)
-{
-	for (Window *c = windows_list(); c; c = c->next)
-		resize(c, wax, way, waw, wah);
-}
