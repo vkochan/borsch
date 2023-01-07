70187a4e8108d0b323c49c2b0f682687e3073379
Vadym Kochan <vadim4j@gmail.com>
2023-01-07 09:18:50 +0200

    main: do not install vt handlers which are not used

--- main.c
+++ main.c
@@ -614,8 +614,6 @@ int term_create(const char *prog, const char *title, const char *cwd, const char
 		free(c);
 		return -1;
 	}
-	vt_urgent_handler_set(process_term_get(proc), term_urgent_handler);
-	vt_title_handler_set(process_term_get(proc), term_title_handler);
 	ui_window_has_title_set(c->win, true);
 	process_buffer_set(proc, c->buf);
 	buffer_proc_set(c->buf, proc);
